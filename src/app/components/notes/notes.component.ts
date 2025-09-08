5e23df950c3a257238522fea55578c2bbc96a42a
Michelle Aijo <michelleaijo@gmail.com>
2025-09-08 14:57:01 +0530

    feat: add Note model and NoteService with basic CRUD

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NoteService } from '../../services/note.service';
+import { Note } from '../../models/note.model';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.css']
 })
-export class NotesComponent {
+export class NoteComponent implements OnInit {
+  notes: Note[] = [];
 
+  constructor(private noteService: NoteService) {}
+
+  ngOnInit(): void {
+    this.notes = this.noteService.getNotes();
+  }
+
+  deleteNote(id: number) {
+    this.noteService.deleteNote(id);
+    this.notes = this.noteService.getNotes();
+  }
 }
+  
